d0f65c93d26bac64c8c92bb9ece3306844db1e0c
PhilippeWekessi <wekessiphilippe@gmail.com>
2026-01-01 06:14:37 -0500

    Update vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/heureuse-annee-2026/',
   plugins: [react()],
-  base: '/heureuse-annee-2026/', 
 })
